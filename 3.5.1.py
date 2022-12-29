fb5da757bbc773d3fae3ff6e970c5117d809c2dd
kr1stos <104152574+kr1stos@users.noreply.github.com>
2022-12-29 22:46:56 +0500

    задание 3.5.1

--- 3.5.1.py
+++ 3.5.1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 
-conn = sqlite3.connect('cur.sqlite')
+conn = sqlite3.connect('currencies.sqlite')
 c = conn.cursor()
 df = pd.read_csv('currencies.csv')
 df = df.to_sql('currencies', conn, if_exists='replace', index=False)
