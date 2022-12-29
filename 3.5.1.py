43a0aee1a8293710423cbec4560ef051b008fe47
kr1stos <104152574+kr1stos@users.noreply.github.com>
2022-12-29 21:43:22 +0500

    задание 3.5.1

---
+++ 3.5.1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sqlite3
+
+
+conn = sqlite3.connect('cur.sqlite')
+c = conn.cursor()
+df = pd.read_csv('currencies.csv')
+df = df.to_sql('currencies', conn, if_exists='replace', index=False)
+conn.commit()
